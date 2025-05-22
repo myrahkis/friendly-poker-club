6d5cb3cc98373cf95874bf18b9fa6dedc043121c
myrahkis <kruzinka@yandex.ru>
2025-05-22 19:46:28 +0300

    faq section

---
+++ components/FAQCard.vue
@@ -0,0 +1,93 @@
+<script setup>
+const { suit, heading, text } = defineProps({
+  suit: String,
+  heading: String,
+  text: String,
+});
+</script>
+
+<template>
+  <div class="faq-card">
+    <div class="card-front">
+      <div class="header">
+        <img class="header-suit" :src="suit" alt="card-suit" />
+        <h4>{{ heading }}</h4>
+      </div>
+      <p>{{ text }}</p>
+      <div class="footer">
+        <img class="footer-suit" :src="suit" alt="card-suit" />
+      </div>
+    </div>
+    <div class="card-back">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
+        iste consequatur nemo fugit. Perspiciatis nulla magni quae eos, tempora
+        iusto earum eius delectus quidem quis nemo, asperiores, a vitae
+        praesentium. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Perspiciatis asperiores reprehenderit illo voluptates praesentium
+        possimus sed repellat, dolorum dolor. Commodi placeat iusto possimus
+        animi perspiciatis qui dignissimos deleniti. Laboriosam, molestias?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aliquam
+        aspernatur quasi fugit consectetur excepturi nostrum, sed, incidunt eos
+        alias esse corrupti nemo odio consequuntur debitis illo assumenda magnam
+        quo.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.faq-card {
+  display: grid;
+  grid-template-areas: "card";
+  width: 100%;
+  height: auto;
+
+  perspective: 150rem;
+  transform-style: preserve-3d;
+  transition: transform 0.8s ease;
+}
+
+.faq-card:hover {
+  transform: rotateY(180deg);
+}
+
+.card-front,
+.card-back {
+  grid-area: card;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: rgba(37, 37, 37, 0.5);
+  backdrop-filter: blur(3px);
+  border: 1px solid var(--light-gradient-color);
+  border-radius: 1.5rem;
+  padding: 2rem;
+  font-size: 1.5rem;
+  backface-visibility: hidden;
+}
+
+.card-front {
+  transform: rotateY(0deg);
+}
+.card-back {
+  transform: rotateY(180deg);
+  align-items: center;
+  justify-content: center;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.footer {
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+}
+.header-suit,
+.footer-suit {
+  width: 5rem;
+}
+</style>
--- components/FAQSection.vue
+++ components/FAQSection.vue
@@ -1,14 +1,69 @@
-<script setup></script>
+<script setup>
+import spade from "@/assets/icons/spades-suit.png";
+import heart from "@/assets/icons/hearts-suit.png";
+import club from "@/assets/icons/clubs-suit.png";
+import diamonds from "@/assets/icons/diamonds-suit.png";
+</script>
 
 <template>
   <section class="faq-section">
     <h2>Вопросы-ответы</h2>
+    <div class="faq-cards">
+      <FAQCard
+        :suit="spade"
+        heading="Заголовок"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius tortor a tincidunt convallis. Fusce consectetur sed leo ut rutrum. Sed ornare, nisl vitae fringilla tristique, nisi ante iaculis nisl, et tincidunt odio leo in leo. Praesent enim orci, iaculis at sem eget, gravida porttitor urna. Mauris a interdum diam. Donec nec faucibus neque. Aenean ac felis vitae tellus viverra blandit sed et nisi. Ut quis tincidunt eros. Nunc sed quam eros. "
+      />
+      <FAQCard
+        :suit="heart"
+        heading="Заголовок"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius tortor a tincidunt convallis. Fusce consectetur sed leo ut rutrum. Sed ornare, nisl vitae fringilla tristique, nisi ante iaculis nisl, et tincidunt odio leo in leo. Praesent enim orci, iaculis at sem eget, gravida porttitor urna. Mauris a interdum diam. Donec nec faucibus neque. Aenean ac felis vitae tellus viverra blandit sed et nisi. Ut quis tincidunt eros. Nunc sed quam eros. "
+      />
+      <FAQCard
+        :suit="club"
+        heading="Заголовок"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius tortor a tincidunt convallis. Fusce consectetur sed leo ut rutrum. Sed ornare, nisl vitae fringilla tristique, nisi ante iaculis nisl, et tincidunt odio leo in leo. Praesent enim orci, iaculis at sem eget, gravida porttitor urna. Mauris a interdum diam. Donec nec faucibus neque. Aenean ac felis vitae tellus viverra blandit sed et nisi. Ut quis tincidunt eros. Nunc sed quam eros. "
+      />
+      <FAQCard
+        :suit="diamonds"
+        heading="Заголовок"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius tortor a tincidunt convallis. Fusce consectetur sed leo ut rutrum. Sed ornare, nisl vitae fringilla tristique, nisi ante iaculis nisl, et tincidunt odio leo in leo. Praesent enim orci, iaculis at sem eget, gravida porttitor urna. Mauris a interdum diam. Donec nec faucibus neque. Aenean ac felis vitae tellus viverra blandit sed et nisi. Ut quis tincidunt eros. Nunc sed quam eros. "
+      />
+    </div>
+    <img class="bg-chip-full-1" src="/assets/images/chip-bg-full.png" alt="" />
+    <img class="bg-chip-full-2" src="/assets/images/chip-bg-full.png" alt="" />
   </section>
 </template>
 
 <style scoped>
 .faq-section {
+  position: relative;
   padding: 0 5rem;
-  height: 100vh;
+  padding-bottom: 5rem;
+  margin-top: 5rem;
+}
+.faq-cards {
+  display: flex;
+  justify-content: center;
+  gap: 2.5rem;
+}
+
+.bg-chip-full-1,
+.bg-chip-full-2 {
+  position: absolute;
+  width: 25rem;
+}
+
+.bg-chip-full-1 {
+  top: 1rem;
+  right: 28rem;
+  transform: rotate(-68deg);
+  z-index: -1;
+}
+.bg-chip-full-2 {
+  left: 38rem;
+  bottom: 0;
+  transform: translateY(5rem);
+  z-index: -1;
 }
 </style>
