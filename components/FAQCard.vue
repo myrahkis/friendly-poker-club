3ac4f5e91ad48eacc67573fea675de1fe647470f
myrahkis <kruzinka@yandex.ru>
2025-05-28 14:43:55 +0300

    fixed faq card's flipping

--- components/FAQCard.vue
+++ components/FAQCard.vue
@@ -8,48 +8,55 @@ const { suit, heading, text } = defineProps({
 
 <template>
   <div class="faq-card">
-    <div class="card-front">
-      <div class="header">
-        <img class="header-suit" :src="suit" alt="card-suit" />
-        <h4>{{ heading }}</h4>
+    <div class="card-inner">
+      <div class="card-front">
+        <div class="header">
+          <img class="header-suit" :src="suit" alt="card-suit" />
+          <h4>{{ heading }}</h4>
+        </div>
+        <p class="text">{{ text }}</p>
+        <div class="footer">
+          <img class="footer-suit" :src="suit" alt="card-suit" />
+        </div>
       </div>
-      <p class="text">{{ text }}</p>
-      <div class="footer">
-        <img class="footer-suit" :src="suit" alt="card-suit" />
+      <div class="card-back">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
+          iste consequatur nemo fugit. Perspiciatis nulla magni quae eos,
+          tempora iusto earum eius delectus quidem quis nemo, asperiores, a
+          vitae praesentium. Lorem ipsum dolor sit, amet consectetur adipisicing
+          elit. Perspiciatis asperiores reprehenderit illo voluptates
+          praesentium possimus sed repellat, dolorum dolor. Commodi placeat
+          iusto possimus animi perspiciatis qui dignissimos deleniti.
+          Laboriosam, molestias? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Eum aliquam aspernatur quasi fugit consectetur
+          excepturi nostrum, sed, incidunt eos alias esse corrupti nemo odio
+          consequuntur debitis illo assumenda magnam quo.
+        </p>
       </div>
-    </div>
-    <div class="card-back">
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
-        iste consequatur nemo fugit. Perspiciatis nulla magni quae eos, tempora
-        iusto earum eius delectus quidem quis nemo, asperiores, a vitae
-        praesentium. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Perspiciatis asperiores reprehenderit illo voluptates praesentium
-        possimus sed repellat, dolorum dolor. Commodi placeat iusto possimus
-        animi perspiciatis qui dignissimos deleniti. Laboriosam, molestias?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aliquam
-        aspernatur quasi fugit consectetur excepturi nostrum, sed, incidunt eos
-        alias esse corrupti nemo odio consequuntur debitis illo assumenda magnam
-        quo.
-      </p>
     </div>
   </div>
 </template>
 
 <style scoped>
 .faq-card {
-  display: grid;
-  grid-template-areas: "card";
   width: 100%;
   height: auto;
   min-height: 45rem;
 
   perspective: 150rem;
+}
+
+.card-inner {
+  display: grid;
+  grid-template-areas: "card";
+  position: relative;
+  width: 100%;
+  height: 100%;
   transform-style: preserve-3d;
   transition: transform 0.8s ease;
 }
-
-.faq-card:hover {
+.faq-card:hover .card-inner {
   transform: rotateY(180deg);
 }
 
@@ -58,7 +65,6 @@ const { suit, heading, text } = defineProps({
   grid-area: card;
   display: flex;
   flex-direction: column;
-  /* justify-content: space-between; */
   background-color: rgba(37, 37, 37, 0.5);
   border: 1px solid var(--light-gradient-color);
   border-radius: 1.5rem;
