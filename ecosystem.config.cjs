14848efb958edf48a5e4229ae224500326ade4b0
myrahkis <kruzinka@yandex.ru>
2025-07-16 23:35:38 +0300

    port & host

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,10 @@ module.exports = {
   apps: [
     {
       name: "friendly-poker-club",
-      port: "4000",
+      env: {
+        NUXT_HOST: "127.0.0.1",
+        NUXT_PORT: "4000",
+      },
       exec_mode: "cluster",
       instances: "max",
       script: "./.output/server/index.mjs",
