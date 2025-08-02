d65bd8329efafe5123a953dd5dbef4b6b0c713b3
myrahkis <kruzinka@yandex.ru>
2025-08-02 19:07:06 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -79,19 +79,19 @@ export default defineNuxtConfig({
   },
   pages: true,
   // experimental!!
-  hooks: {
-    "build:manifest": (manifest) => {
-      // find the app entry, css list
-      const css = Object.values(manifest).find(
-        (options) => options.isEntry
-      )?.css;
-      if (css) {
-        // start from the end of the array and go to the beginning
-        for (let i = css.length - 1; i >= 0; i--) {
-          // if it starts with 'entry', remove it from the list
-          if (css[i].startsWith("entry")) css.splice(i, 1);
-        }
-      }
-    },
-  },
+  // hooks: {
+  //   "build:manifest": (manifest) => {
+  //     // find the app entry, css list
+  //     const css = Object.values(manifest).find(
+  //       (options) => options.isEntry
+  //     )?.css;
+  //     if (css) {
+  //       // start from the end of the array and go to the beginning
+  //       for (let i = css.length - 1; i >= 0; i--) {
+  //         // if it starts with 'entry', remove it from the list
+  //         if (css[i].startsWith("entry")) css.splice(i, 1);
+  //       }
+  //     }
+  //   },
+  // },
 });
